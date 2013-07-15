10eb7dd00d2ebac024cdb77d37be6fa033369b17
Anton Van Oosbree <vanoosbree@gmail.com>
2013-07-15 13:08:13 -0700

    "Initial link navigation"

--- epstein/urls.py
+++ epstein/urls.py
@@ -7,25 +7,29 @@ urlpatterns = patterns('',
   
     # homepage routes
     url( r'^$', 			'apps.home.views.index',  name='home' ),
-    url( r'^home/$', 		'apps.home.views.home',   name='home' ),
+    url( r'^logout$',       'apps.home.views.index',  name='logout' ),
+
 
     #users routes
     url( r'^users/$', 	 	'apps.users.views.index', 	name='home' ),
     url( r'^users/new$', 	'apps.users.views.new', 	name='home' ),
     url( r'^users/login$', 	'apps.users.views.login', 	name='home' ),
+    url( r'^dashboard/$',   'apps.users.views.dashboard',   name='bands' ),
 
     #bands routes
-    url( r'^dashboard/$', 	 'apps.users.views.dashboard', 	name='bands' ),
+    url( r'^bands/$',       'apps.bands.views.show_bands',  name='show_bands' ),
+   
+    # event routes
+    url( r'^events/$',      'apps.events.views.events',  name='home' ),
 
+    # setlist routes
+    url( r'^setlist/$',     'apps.setlists.views.setlist',  name='setlist' ),
 
     #login routes
     url( r'^users/$', 		'apps.login.urls',  name='home' ),
 
     # basically hacks for now... need to clean up
     url( r'^about/$', 		'apps.users.views.about',  name='home' ),
-    url( r'^bands/$', 		'apps.users.views.bands',  name='home' ),
-    url( r'^events/$', 		'apps.users.views.events',  name='home' ),
-    url( r'^setlist/$', 	'apps.users.views.setlist',  name='home' ),
 
     # admin routes
     #url( r'^admin/doc/', include('django.contrib.admindocs.urls') ),
